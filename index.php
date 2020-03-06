2430d9e00adf0e0e7415259a822a53435d70b9e7
larasn6706174044 <larasn@student.telkomuniversity.ac.id>
2020-03-06 13:58:11 +0700

    landing page ui polish + modal template

--- index.php
+++ index.php
@@ -7,17 +7,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LKPS - LOGIN</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.20/datatables.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
 </head>
 
 <body>
     <div class="flex-container">
         <div class="left-container" style="flex-grow: 8">
-            <img src="aset/group.png" style="width: 520px" alt="">
+            <img src="aset/index_img.svg" style="width:90%;margin-left:10%" alt="">
         </div>
         <div style="flex-grow: 8">
             <div class="right-container">
                 <div class="text-content">
-                    <h1>LKPS</h1>
+                    <div>L K P S</div>
                     <p>Laporan Kinerja Program Studi <br> D3 Rekayasa Perangkat Lunak Aplikasi <br> Telkom University
                 </div>
                 <p class="as">Masuk sebagai...</p>
