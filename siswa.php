a479266bc7f33c0205a04a44e1cd7efc79d6cbda
larasn6706174044 <larasn@student.telkomuniversity.ac.id>
2020-03-12 08:33:57 +0700

    ui polish

--- siswa.php
+++ siswa.php
@@ -20,16 +20,23 @@
     <thead>
         <tr>
             <th >No</th>
+            <th >Email</th>
             <th >Nama</th>
             <th >NIM</th>
             <th >Bulan dan Tahun Lulus</th>
-            <th Bulan dan Tahun Pertama Kerja</th>
+            <th >Bulan dan Tahun Kerja</th>
             <th >Nama Perusahaan</th>
             <th >Nama Wirausaha</th>
             <th >Tanggal Wirausaha</th>
             <th >Link Kartu Pegawai</th>
             <th >Bidang Pekerjaan</th>
-
+            <th >Cakupan Area Kerja</th>
+            <th >Nama Atasan</th>
+            <th >Kontak Atasan</th>
+            <th >Masukan</th>
+            <th >Kesediaan Hadir</th>
+            <th >Pendapatan Perbulan</th>
+            <th >Kesesuaian Pendidikan</th>
         </tr>
     </thead>";
     echo"<tbody>";
@@ -38,10 +45,23 @@
     {   
         echo "<tr>";
             echo "<td align=center style='width:50px;'>$i</td>";
-            echo "<td style='width:200px;'>$data[1]</td>";
-            // echo "<td style='width:300px;'>$data[2]</td>";
-            // echo "<td style='width:300px;'>$data[3]</td>";
-            // echo "<td style='width:100px;'>$data[4]</td>";
+            echo "<td style='width:300px;'>$data[2]</td>";
+            echo "<td style='width:300px;'>$data[3]</td>";
+            echo "<td style='width:100px;'>$data[4]</td>";
+            echo "<td style='width:100px;'>$data[5]</td>";
+            echo "<td style='width:100px;'>$data[6]</td>";
+            echo "<td style='width:100px;'>$data[7]</td>";
+            echo "<td style='width:100px;'>$data[8]</td>";
+            echo "<td style='width:100px;'>$data[9]</td>";
+            echo "<td style='width:100px;'>$data[10]</td>";
+            echo "<td style='width:100px;'>$data[11]</td>";
+            echo "<td style='width:100px;'>$data[12]</td>";
+            echo "<td style='width:100px;'>$data[13]</td>";
+            echo "<td style='width:100px;'>$data[14]</td>";
+            echo "<td style='min-width:400px;'>$data[15]</td>";
+            echo "<td style='width:100px;'>$data[16]</td>";
+            echo "<td style='width:100px;'>$data[17]</td>";
+            echo "<td style='width:100px;'>$data[18]</td>";
             // echo "<td style='width:150px;'>";
             // if($data[1] == $_SESSION['kode_dosen']){
             //     echo "<a href='download.php?file_id=$data[0]'><i class='fas fa-download' style='color: #000000; padding-right:10px; padding-left: 5px'></i></a>";
